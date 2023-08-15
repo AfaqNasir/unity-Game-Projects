dae6b853090d9bfc0e13ac1531e8841b6dc897ba
devops-get3ms <devops@getg3ms.com>
2023-08-15 13:43:27 +0500

    2nd game

---
+++ Assets/CheckCollision.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckCollision : MonoBehaviour {
+
+
+    float vel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
+
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+
+            vel = collision.gameObject.GetComponent<Rigidbody2D>().velocity.y;
+            if (vel > -0.2f)
+            {
+
+                Debug.Log("Good Landing");
+
+            }
+            else
+            {
+                Debug.Log("Bad Landing");
+
+            }
+
+
+        }
+    }
+
+
+
+
+}
---
+++ Assets/LanderControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanderControl : MonoBehaviour {
+
+    Rigidbody2D rigid;
+    public GameObject Lander;
+
+	// Use this for initialization
+	void Start () {
+        rigid = Lander.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       // Debug.Log(rigid.velocity.y);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            if (rigid.velocity.y < 5)
+                rigid.AddForce(new Vector2(0, 2));
+        
+        
+        
+        }
+
+	
+	}
+}
